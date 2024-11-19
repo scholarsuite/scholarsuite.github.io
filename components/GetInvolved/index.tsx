50e23b6289f76cbd00abbbe11b45d3dd47953b2e
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-11-19 21:41:49 +0100

    fix code format

--- components/GetInvolved/index.tsx
+++ components/GetInvolved/index.tsx
@@ -23,57 +23,49 @@ const GetInvolved: FC = async () => {
         <div className={styles.card}>
           <CodeBracketIcon />
           <h3>{t('cards.contribute.title')}</h3>
-          <p>
-            {t('cards.contribute.description')}
-          </p>
+          <p>{t('cards.contribute.description')}</p>
           <Link href="https://github.com/AugustinMauroy/scholarsuite/issues?q=is%3Aopen+is%3Aissue+label%3A%22good+first+issue%22">
-            {t("cards.learnMore")} <ArrowRightIcon />
+            {t('cards.learnMore')} <ArrowRightIcon />
           </Link>
         </div>
         <div className={styles.card}>
           <BoltIcon />
-          <h3>{t("cards.testApp.title")}</h3>
-          <p>
-          {t("cards.testApp.description")}
-          </p>
+          <h3>{t('cards.testApp.title')}</h3>
+          <p>{t('cards.testApp.description')}</p>
           <Link href="https://github.com/AugustinMauroy/scholarsuite?tab=readme-ov-file#having-your-instance-of--scholarsuite-">
-          {t("cards.learnMore")} <ArrowRightIcon />
+            {t('cards.learnMore')} <ArrowRightIcon />
           </Link>
         </div>
         <div className={styles.card}>
           <BugAntIcon />
-          <h3>{t("cards.reportBugs.title")}</h3>
-          <p>{t("cards.reportBugs.description")}</p>
+          <h3>{t('cards.reportBugs.title')}</h3>
+          <p>{t('cards.reportBugs.description')}</p>
           <Link href="https://github.com/AugustinMauroy/scholarsuite/issues">
-          {t("cards.learnMore")} <ArrowRightIcon />
+            {t('cards.learnMore')} <ArrowRightIcon />
           </Link>
         </div>
         <div className={styles.card}>
           <SparklesIcon />
-          <h3>{t("cards.featureRequests.title")}</h3>
-          <p>{t("cards.featureRequests.description")}</p>
+          <h3>{t('cards.featureRequests.title')}</h3>
+          <p>{t('cards.featureRequests.description')}</p>
           <Link href="https://github.com/AugustinMauroy/scholarsuite/issues">
-          {t("cards.learnMore")} <ArrowRightIcon />
+            {t('cards.learnMore')} <ArrowRightIcon />
           </Link>
         </div>
         <div className={styles.card}>
           <ChatBubbleLeftEllipsisIcon />
-          <h3>{t("cards.explainInterest.title")}</h3>
-          <p>
-            {t("cards.explainInterest.description")}
-          </p>
+          <h3>{t('cards.explainInterest.title')}</h3>
+          <p>{t('cards.explainInterest.description')}</p>
           <Link href="#">
-          {t("cards.learnMore")} <ArrowRightIcon />
+            {t('cards.learnMore')} <ArrowRightIcon />
           </Link>
         </div>
         <div className={styles.card}>
           <HeartIcon />
-          <h3>{t("cards.supportProject.title")}</h3>
-          <p>
-            {t("cards.supportProject.description")}
-          </p>
+          <h3>{t('cards.supportProject.title')}</h3>
+          <p>{t('cards.supportProject.description')}</p>
           <Link href="https://github.com/AugustinMauroy/scholarsuite">
-          {t("cards.learnMore")} <ArrowRightIcon />
+            {t('cards.learnMore')} <ArrowRightIcon />
           </Link>
         </div>
       </div>
