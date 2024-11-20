654c3db9a81b2021d55291092c2fba19bc308f3b
Augustin Mauroy <augustin.mauroy@outlook.fr>
2024-11-20 12:41:59 +0100

    feat(SEO): optimize (#12)

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,21 @@
-import { setRequestLocale } from 'next-intl/server';
+import { setRequestLocale, getTranslations } from 'next-intl/server';
 import Footer from '~/components/Footer';
 import GetInvolved from '~/components/GetInvolved';
 import Hero from '~/components/Hero';
 import type { FC } from 'react';
+import type { Metadata } from 'next';
 import type { BaseParams } from '~/types/params';
 
 type PageProps = BaseParams;
+
+export const generateMetadata = async (): Promise<Metadata> => {
+  const t = await getTranslations('app.home.metadata');
+
+  return {
+    title: t('title'),
+    description: t('description'),
+  };
+};
 
 const Page: FC<PageProps> = async ({ params }) => {
   const { locale } = await params;
